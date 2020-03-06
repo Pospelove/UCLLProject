4b1aeca946c6fc575001a1e07bdf4c856d14406a
Wannes Verschraegen <r0807534@ucll.be>
2020-03-06 17:18:39 +0100

    Laaste Test

--- GIP1.Web/Program.cs
+++ GIP1.Web/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace GIP1.Web
 {
-    public class Program//test
+    public class Program//testt
     {
         public static void Main(string[] args)
         {
