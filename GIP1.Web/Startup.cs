ed0d8bc1ee2d2754408c4870e1c213f1d3bdbe53
Wannes Verschraegen <r0807534@ucll.be>
2020-03-06 14:26:37 +0100

    Test

--- GIP1.Web/Startup.cs
+++ GIP1.Web/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace GIP1.Web
 {
-    public class Startup//tesdt
+    public class Startup//t
     {
         public Startup(IConfiguration configuration)
         {
